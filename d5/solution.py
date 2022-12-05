4e6c191433bb92d88946c741145b0a596bb47ea3
Casper <draakie@protonmail.com>
2022-12-05 16:47:21 +0100

    Day 5 part 2.

--- d5/solution.py
+++ d5/solution.py
@@ -1,17 +1,26 @@
+import copy
+
 class Move():
     def __init__(self, count, fromIndex, toIndex):
         self.count = count
-        self.fromIndex = fromIndex
-        self.toIndex = toIndex
+        self.fromIndex = fromIndex - 1
+        self.toIndex = toIndex - 1
 
     def __str__(self):
-        return f"Move {self.count} from {self.fromIndex} to {self.toIndex}"
+        return f"Move {self.count} from {self.fromIndex + 1} to {self.toIndex + 1}"
 
-    def apply(self, stacks):
+    def apply1(self, stacks):
+        fromStack = stacks[self.fromIndex]
+        toStack = stacks[self.toIndex]
         for i in range(0, self.count):
-            fromStack = stacks[self.fromIndex - 1]
-            toStack = stacks[self.toIndex - 1]
             toStack.push(fromStack.pop())
+
+    def apply2(self, stacks):
+        fromStack = stacks[self.fromIndex]
+        toStack = stacks[self.toIndex]
+        movingStack = fromStack.slice(self.count)
+        for i in range(0, self.count):
+            toStack.push(movingStack[i])
 
 class StackOfCrates():
     def __init__(self, index, stack):
@@ -32,10 +41,12 @@ class StackOfCrates():
     def peek(self):
         return self.stack[-1]
 
-def main():
-    with open('input.txt', 'r') as file:
-        lines = list(map(lambda l: l.strip('\n'), file.readlines()))
+    def slice(self, count):
+        crates = self.stack[-count:]
+        self.stack = self.stack[0:len(self.stack) - count]
+        return crates
 
+def parse_stacks(lines):
     # Figure out number of stacks and initialize.
     numberOfStacks = (len(lines[0].strip('\n')) // 4) + 1
     stacks = [StackOfCrates(x, []) for x in range(1, numberOfStacks + 1)]
@@ -47,6 +58,7 @@ def main():
         lineIndex += 1
         line = lines[lineIndex]
 
+    # Save index to read moves from later.
     moveIndex = lineIndex + 2
 
     # Parse stacks in reverse (stack order).
@@ -60,23 +72,47 @@ def main():
                 stacks[stackIndex].push(char)
             stackIndex = stackIndex + 1
 
-    # Parse moves.
+    return stacks, moveIndex
+
+def parse_moves(lines, moveIndex):
     moves = []
     for lineIndex in range(moveIndex, len(lines)):
         line = lines[lineIndex][5:]
         ints = list(map(int, line.split()[0::2]))
         moves.append(Move(ints[0], ints[1], ints[2]))
+    return moves
 
-    # Apply moves.
+def main():
+    with open('input.txt', 'r') as file:
+        lines = list(map(lambda l: l.strip('\n'), file.readlines()))
+
+    # Parse stacks and moves.
+    stacks, moveIndex = parse_stacks(lines)
+    moves = parse_moves(lines, moveIndex)
+
+    # Save a copy of stacks for part 2.
+    stacksCopy = copy.deepcopy(stacks)
+
+    # Part 1.
     for move in moves:
-        move.apply(stacks)
+        move.apply1(stacks)
 
-    # Report top crates.
     result = ''
     for stack in stacks:
-        result = result + stack.peek()
+        result += stack.peek()
 
-    print(f"The top crates are {result}.")
+    print(f"Using CrateMover9000 the top crates are {result}.")
+
+    # Part 2.
+    stacks = stacksCopy
+    for move in moves:
+        move.apply2(stacks)
+
+    result = ''
+    for stack in stacks:
+        result += stack.peek()
+
+    print(f"Using CrateMover9001 the top crates are {result}.")
 
 if __name__ == '__main__':
     main()
